7acf196a9c105adc6226558490993f580aed0bae
Jalison Sousa <140900161+jalisonsousa@users.noreply.github.com>
2023-08-19 19:41:44 -0400

    falha de segurança corrigido por R0HTG0R/KASPAROV

--- download.php
+++ download.php
@@ -1,19 +1,14 @@
 <?php
-// FALHA DE SEGURANÇA: 19/08/2023
 
-// Verifica se o parâmetro "arquivo" está presente na URL
 if (isset($_GET['arquivo'])) {
   $arquivo = $_GET['arquivo'];
   if($arquivo == ""){ echo "√ Você é malvado? kkk " . $_SERVER["REMOTE_ADDR"]; exit; ; }
   
-  // Caminho para a pasta de uploads
   $caminhoArquivo = 'arquivos/' . $arquivo;
   $caminhoCheck = "../";
 
-  // Verifica se o arquivo existe
   if (strpos($caminhoArquivo, $caminhoCheck) === false) {
     if(file_exists($caminhoArquivo)){
-      // Define os cabeçalhos para forçar o download
       header('Content-Description: File Transfer');
       header('Content-Type: application/octet-stream');
       header('Content-Disposition: attachment; filename=' . basename($caminhoArquivo));
@@ -31,5 +26,5 @@ if (isset($_GET['arquivo'])) {
   }
 } else {
   echo 'O arquivo não foi especificado.';
-} // RESOLVIDO POR R0HTG0R/KASPAROV
+}
 ?>
