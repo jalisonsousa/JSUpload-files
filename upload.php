c120eaadb2f5c296111018b09a57d3355fd5c205
Jalison Sousa <sjalison502@gmail.com>
2023-08-19 20:49:55 -0400

    falha de segurança corrigido por 1999x

--- upload.php
+++ upload.php
@@ -4,19 +4,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
   $fileName = $file['name'];
   $fileTmp = $file['tmp_name'];
   $fileType = $file['type'];
+  $allowedTypes = array('image/jpg', 'image/jpeg', 'image/png', 'application/pdf');
 
-  // Check if it's an image or PDF file
-  $allowedTypes = array('image/jpeg', 'image/png', 'application/pdf');
   if (!in_array($fileType, $allowedTypes)) {
     http_response_code(400);
     die('Tipo de arquivo inválido. Apenas imagens (JPEG, PNG) e PDFs são permitidos.');
   }
+  $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+  if (!in_array($fileExtension, array('jpg', 'jpeg', 'png', 'pdf'))) {
+    http_response_code(400);
+    die('Extensão de arquivo não permitida. Apenas JPG, JPEG, PNG e PDF são permitidos.');
+  }
 
-  // Specify the upload directory
-  $uploadDir = 'arquivos/';
+  $uploadDir = 'uploads/';
   $filePath = $uploadDir . $fileName;
 
-  // Move the uploaded file to the desired location
   if (move_uploaded_file($fileTmp, $filePath)) {
     http_response_code(200);
     echo 'Arquivo enviado com sucesso!';
