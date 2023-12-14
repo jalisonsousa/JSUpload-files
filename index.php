34a16e51131d42ab50750d7afb383a41495dc3cb
Jalison Sousa <140900161+jalisonsousa@users.noreply.github.com>
2023-12-14 14:01:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     </center>
     <div class="footer mt-5">
       <a href="https://github.com/jalisonsousa">
-        <img src="https://cdn.discordapp.com/attachments/410233690212401162/1128038777533780060/1689008345978.png" alt="JalisonSousa" class="footer-image" />
+        <img src="https://jalisonbr.com/img/jalisonbrLogo.jpg" alt="JalisonSousa" class="footer-image" />
       </a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
